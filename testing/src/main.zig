ab361128b983c84a5b8f3296546291f1d12b894f
Benjamin <60051088+larkben@users.noreply.github.com>
2026-06-12 22:20:44 +0000

    testing with pointers

--- testing/src/main.zig
+++ testing/src/main.zig
@@ -1,6 +1,19 @@
 const std = @import("std");
 const Io = std.Io;
 
+const Tree = struct {
+    name: []const u8,
+
+    pub fn initTree(name: []const u8) Tree {
+        return Tree{ .name = name };
+    }
+
+    pub fn changeName(self: *Tree) void {
+        self.name = "cherry";
+        std.debug.print("The new name is now: {s}\n", .{self.name});
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     // Prints to stderr, unbuffered, ignoring potential errors.
     const allocator = init.gpa;
@@ -11,4 +24,15 @@ pub fn main(init: std.process.Init) !void {
     try list.append(allocator, 'a');
 
     std.debug.print("{c}\n", .{list.items[0]});
+
+    testPointer();
+
+    var a: Tree = Tree.initTree("apple");
+    a.changeName();
+}
+
+fn testPointer() void {
+    const x: i32 = 1234;
+    const x_ptr = &x;
+    std.debug.print("{d}\n", .{x_ptr.*}); // dereference
 }
